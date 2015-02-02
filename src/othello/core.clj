05994ec1556f613141882ac8df427f2425b2499e
Tomas Ekholm <ekholm.tomas@gmail.com>
2015-02-02 16:12:45 +0100

    Ability to make moves added

--- src/othello/core.clj
+++ src/othello/core.clj
@@ -5,16 +5,13 @@
   #^{:doc  "Creates a square board of the given even size for two players."
      :test (fn []
              (is (= (square-board 4 ["W" "B"])
-                    {[0 0] "." [1 0] "." [2 0] "." [3 0] "."
-                     [0 1] "." [1 1] "W" [2 1] "B" [3 1] "."
-                     [0 2] "." [1 2] "B" [2 2] "W" [3 2] "."
-                     [0 3] "." [1 3] "." [2 3] "." [3 3] "."}))
-             (is (= (get (square-board 4 ["W" "B"] "x") [0 0]) "x"))
+                    {[0 0] nil [1 0] nil [2 0] nil [3 0] nil
+                     [0 1] nil [1 1] "W" [2 1] "B" [3 1] nil
+                     [0 2] nil [1 2] "B" [2 2] "W" [3 2] nil
+                     [0 3] nil [1 3] nil [2 3] nil [3 3] nil}))
              (is (thrown? IllegalArgumentException (square-board 5)))
              (is (thrown? IllegalArgumentException (square-board 6 ["W" "B" "R"]))))}
-  square-board
-  ([board-size players] (square-board board-size players "."))
-  ([board-size players free]
+  square-board [board-size players]
     (do
       (when (odd? board-size) (throw (IllegalArgumentException. "board-size must be even")))
       (when (not= (count players) 2) (throw (IllegalArgumentException. "there must be two players")))
@@ -29,15 +26,15 @@
                      (or
                        (and (= x middle-) (= y middle+))
                        (and (= x middle+) (= y middle-))) [[x y] (second players)]
-                     :else [[x y] free])))))))
+                     :else [[x y] nil]))))))
 
 (defn
   #^{:doc  "Creates a board from the given string arguments. Nodes can be left out with space characters."
      :test (fn []
              (is (= (string-to-board '("W" "B") '("0..1" " 0.." "111.1"))
-                    {[0 0] "W" [1 0] "." [2 0] "." [3 0] "B"
-                     [1 1] "W" [2 1] "." [3 1] "."
-                     [0 2] "B" [1 2] "B" [2 2] "B" [3 2] "." [4 2] "B"})))}
+                    {[0 0] "W" [1 0] nil [2 0] nil [3 0] "B"
+                     [1 1] "W" [2 1] nil [3 1] nil
+                     [0 2] "B" [1 2] "B" [2 2] "B" [3 2] nil [4 2] "B"})))}
   string-to-board
   ([players string-board]
     (let [map-strategy (fn [value] (nth players (read-string value)))]
@@ -53,7 +50,7 @@
                            occupant (str (second x-and-occupant))]]
                  (condp = occupant
                    " " ()
-                   "." [[x y] occupant]
+                   "." [[x y] nil]
                    [[x y] (map-strategy occupant)]))))))
 
 (defn
@@ -62,24 +59,24 @@
              (is (simple-string-to-board ".BW"
                                          " BBW"
                                          "W. B")
-                 {[0 0] "." [1 0] "B" [2 0] "W"
+                 {[0 0] nil [1 0] "B" [2 0] "W"
                   [1 1] "B" [2 1] "B" [3 1] "W"
-                  [0 2] "W" [1 2] "." [3 2] "B"}))}
+                  [0 2] "W" [1 2] nil [3 2] "B"}))}
   simple-string-to-board [& string-board]
   (string-to-board '() string-board identity))
 
 (defn
   #^{:doc  "Creates a diamond board for three players."
      :test (fn []
-             (is (= (board-to-string (diamond-board "W" "B" "R")) (clojure.string/join '("    .    \n"
-                                                                                          "   ...   \n"
-                                                                                          "  .....  \n"
-                                                                                          " ..WBR.. \n"
-                                                                                          "...RWB...\n"
-                                                                                          " ..BRW.. \n"
-                                                                                          "  .....  \n"
-                                                                                          "   ...   \n"
-                                                                                          "    .    \n")))))}
+             (is (= (diamond-board "W" "B" "R") (simple-string-to-board "    .    "
+                                                                        "   ...   "
+                                                                        "  .....  "
+                                                                        " ..WBR.. "
+                                                                        "...RWB..."
+                                                                        " ..BRW.. "
+                                                                        "  .....  "
+                                                                        "   ...   "
+                                                                        "    .    "))))}
   diamond-board [& players]
   (do
     (when (not= (count players)) (throw IllegalArgumentException "There must be three players"))
@@ -93,16 +90,28 @@
                                "   ..."
                                "    ."))))
 
-(defn-
+(defn
+  #^{:doc  "Determines if the given board contains the given coordinate."
+     :test (fn []
+             (let [board (simple-string-to-board ".W")]
+              (is (true? (contains-coordinate board 0 0)))
+              (is (false? (contains-coordinate board 1 1)))))}
+  contains-coordinate [board x y]
+  (contains? board [x y]))
+
+(defn
   #^{:doc  "Acquire the occupant player id at the given coordinate."
      :test (fn []
              (let [board {[0 0] "." [1 0] "W" [2 0] "."
                           [0 1] "B" [1 1] "W" [2 1] "W"}]
                (is (= (get-occupant board 0 1) "B"))
-               (is (= (get-occupant board 2 0) "."))
-               (is (= (get-occupant board 3 3) nil))))}
+               (is (= (get-occupant board 2 0) nil))
+               (is (thrown? IllegalArgumentException (get-occupant board 3 3)))))}
   get-occupant [board x y]
-  (get board [x y]))
+  (cond
+    (and (contains? board [x y]) (not= (get board [x y]) ".")) (get board [x y])
+    (contains? board [x y]) nil
+    :else (throw (IllegalArgumentException. "The board does not contain the given coordinate."))))
 
 (defn-
   #^{:doc  "Returns the max value of all coordinates n-th position"
@@ -117,47 +126,121 @@
   #^{:doc  "A nice string version of the board."
      :test (fn []
              (let [board (simple-string-to-board ".W."
-                                                 "BW"
+                                                 " BWB"
                                                  "W")]
-               (is (= (board-to-string board) ".W.\nBW \nW  \n"))))}
+               (is (= (board-to-string board) ".W. \n BWB\nW   \n"))))}
   board-to-string [board]
   (let [max-x (max-coordinate board 0)
         max-y (max-coordinate board 1)]
     (apply str (for
                  [y (range (inc max-y))
                   x (range (inc max-x))
-                  :let [occupant (get-occupant board x y)]]
+                  :let [max-x? (= x max-x)
+                        contains-coordinate? (contains-coordinate board x y)]]
                  (cond
-                   (and (nil? occupant) (= max-x x)) " \n"
-                   (nil? occupant) " "
-                   (= max-x x) (str occupant "\n")
-                   :else occupant)))))
+                   (and (not contains-coordinate?) max-x?) " \n"
+                   (not contains-coordinate?) " "
+                   (nil? (get-occupant board x y)) "."
+                   (= max-x x) (str (get-occupant board x y) "\n")
+                   :else (get-occupant board x y))))))
 
-(defn-
+(defn
   #^{:doc  "Returns true if a player is occupying the node at the given coordinates."
      :test (fn []
              (let [board (simple-string-to-board ".WB")]
                (is (false? (is-marked board 0 0)))
-               (is (true? (is-marked board 1 0)))
-               (is (true? (is-marked board "_" 0 0)))))}
-  is-marked
-  ([board x y] (is-marked board "." x y))
-  ([board free x y]
-    (let [occupant? (get-occupant board x y)]
-      (not= free occupant?))))
+               (is (true? (is-marked board 1 0)))))}
+  is-marked [board x y]
+  (let [occupant? (get-occupant board x y)]
+    (not (nil? occupant?))))
 
 (defn-
   #^{:doc  "If the the board is not marked at the position [x y] then the board is updated with the player marked at [x y]"
      :test (fn []
-             (let [board
-                   (simple-string-to-board ".W."
-                                           "BW.")]
-               (is (mark board "X" 0 0)
-                   (simple-string-to-board "XW."
-                                           "BW."))
-               (is (thrown? IllegalArgumentException (mark board "X" 1 0)))))}
+             (let [board (simple-string-to-board ".W."
+                                                 "BW.")]
+               (is (= (mark board "X" 0 0)
+                      (simple-string-to-board "XW."
+                                              "BW.")))
+               (is (thrown? IllegalArgumentException (mark board "X" 3 0)))))}
   mark [board player x y]
   (update-in board [[x y]] (fn [node]
-                             (if (not= node ".")
-                               (throw (IllegalArgumentException. ""))
-                               :player))))
+                             (if (not (contains-coordinate board x y))
+                               (throw (IllegalArgumentException. "The board does not contain the given coordinate."))
+                               player))))
+
+(defn-
+  #^{:doc  "Updates the board in the given direction if player moves at coordinate [x y]."
+     :test (fn []
+             (let [board (simple-string-to-board "..WWB"
+                                                 "..BB."
+                                                 ".....")]
+               (is (= (move-in-direction board "B" 1 0 1 0)
+                      (simple-string-to-board "..BBB"
+                                              "..BB."
+                                              ".....")))
+               (is (= board (move-in-direction board "B" 0 0 1 0)))
+               (is (= board (move-in-direction board "B" 0 0 -1 0)))
+               (is (= (move-in-direction board "W" 1 2 1 -1)
+                      (simple-string-to-board "..WWB"
+                                              "..WB."
+                                              ".....")))
+               (is (thrown? IllegalArgumentException (move-in-direction board "W" 5 0 -1 0)))))}
+  move-in-direction [board player x y dx dy]
+  (let [swap-pieces-in-direction (fn [board player x y dx dy]
+                                   (let [new-x (+ x dx)
+                                         new-y (+ y dy)
+                                         contains-coordinate? (contains-coordinate board new-x new-y)]
+                                     (cond
+                                       (not contains-coordinate?) nil
+                                       (nil? (get-occupant board new-x new-y)) nil
+                                       (= player (get-occupant board new-x new-y)) board
+                                       :else (recur (mark board player new-x new-y) player new-x new-y dx dy))))
+        new-board (swap-pieces-in-direction board player x y dx dy)]
+    (if (contains-coordinate board x y)
+      (cond
+        (= new-board nil) board
+        :else new-board)
+      (throw (IllegalArgumentException. "The board does not contain the given coordinate.")))))
+
+(defn
+  #^{:doc  "Updates the board with the given move."
+     :test (fn []
+             (let [board (simple-string-to-board ".BW."
+                                                 "BB.."
+                                                 "BBB."
+                                                 "WWWW")]
+               (is (= (move board "W" 0 0) (simple-string-to-board "WWW."
+                                                                   "WW.."
+                                                                   "WBW."
+                                                                   "WWWW")))
+               (is (= (move board "B" 3 0) (simple-string-to-board ".BBB"
+                                                                   "BB.."
+                                                                   "BBB."
+                                                                   "WWWW")))
+               (is (thrown? IllegalArgumentException (move board "B" 0 0)))
+               (is (thrown? IllegalArgumentException (move board "B" 1 0)))))}
+  move [board player x y]
+  (let [directions '([-1 -1] [-1 0] [-1 1] [0 -1] [0 1] [1 -1] [1 0] [1 1])
+        new-board (reduce #(move-in-direction %1 player x y (first %2) (second %2)) board directions)]
+    (if (not= board new-board)
+      (mark new-board player x y)
+      (throw (IllegalArgumentException. "Can not move at that coordinate")))))
+
+(defn
+  #^{:doc  "Determines if the given player has a valid move."
+     :test (fn []
+             (let [board (simple-string-to-board "..BW")]
+               (is (true? (has-valid-move board "W")))
+               (is (false? (has-valid-move board "B")))))}
+  has-valid-move [board player]
+  (if (some
+        #(and
+          (not (is-marked board (first %) (second %)))
+          (try
+            (when (move board player (first %) (second %)) true)
+            (catch Exception e false)))
+            ;(not= board (move board player (first %) (second %))))
+        (keys board))
+    true
+    false))

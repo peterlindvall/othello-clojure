2bc8fb35ae4c1db2b294116d049e4f61d09c0faa
Lucas Wiener <lucas@wiener.se>
2015-03-04 10:33:58 +0100

    Added test/all.clj that will run all tests in our namespaces. Also works with lein test

---
+++ test/all.clj
@@ -0,0 +1,14 @@
+(ns all
+  (:use clojure.test))
+
+(deftest all
+  (let [namespaces '(
+                      "othello.board"
+                      "othello.core"
+                      "othello.mutator"
+                      "othello.state"
+                      "test.core"
+                      "util.core"
+                      )]
+    (apply require (map symbol namespaces))
+    (is (successful? (apply run-tests (map symbol namespaces))))))

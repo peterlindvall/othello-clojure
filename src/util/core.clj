d06106c4f1306f1f3d67886a66c61fc1644ad299
Lucas Wiener <lucas@wiener.se>
2015-03-08 13:49:24 +0100

    util/core/->value: Added option depth parameter.

--- src/util/core.clj
+++ src/util/core.clj
@@ -7,7 +7,10 @@
 
 (defn
   #^{:doc "Works like clojure.core/deref except that if the given value is not something dereferable,
-          the value will simply be returned. If the given value is a collection, it will be traversed and dereferenced."
+          the value will simply be returned. If the given value is a collection, it will be traversed and dereferenced.
+          The option depth argument tells how deep the algorithm should go in the structure.
+          Depth of 0 will return the exact object given as argument.
+          Default depth is infinity."
      :test (fn []
              (is= (->value "test") "test")
              (is= (->value (atom "test")) "test")
@@ -17,13 +20,21 @@
              (is= (->value {:test (ref "test")}) {:test "test"})
              (is= (->value {:outer {:test (ref "test")}}) {:outer {:test "test"}})
              (is= (->value {:outer (ref {:test (ref "test")})}) {:outer {:test "test"}})
-             (is= (->value (list (atom {:very (ref "nested")}) #{(atom "so") "nested"})) (list {:very "nested"} #{"so" "nested"})))}
-  ->value [object]
-  (postwalk (fn [value]
-              (if (instance? clojure.lang.IDeref value)
-                (->value @value)
-                value))
-            object))
+             (is= (->value (list (atom {:very (ref "nested")}) #{(atom "so") "nested"})) (list {:very "nested"} #{"so" "nested"}))
+             (let [inner {:test (ref "test")}
+                   outer {:outer (ref inner)}]
+               (is= (->value outer 0) outer)
+               (is= (->value outer 1) {:outer inner})))}
+  ->value
+  ([object] (->value object Double/POSITIVE_INFINITY))
+  ([object depth]
+    (if (= depth 0)
+      object
+      (postwalk (fn [value]
+                  (if (instance? clojure.lang.IDeref value)
+                    (->value @value (dec depth))
+                    value))
+                object))))
 (defn
   #^{:doc "Generates a random UUID."}
   uuid []
